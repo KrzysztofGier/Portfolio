38791bde9e8d8b7234dc3afc7e7c7e5d6af3bbad
Krzysztof Gierczak <krzysztof.gierczak91@gmail.com>
2022-08-14 23:32:56 +0200

    Final

--- infos/views.py
+++ infos/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
 def home(request):
-    return render(request, "infos/home.html")
+    return render(request, "infos\home.html")
 
 def me(request):
-    return render(request, "infos/me.html")
+    return render(request, "infos\me.html")
 
 def contact(request):
-    return render(request, "infos/contact.html")
+    return render(request, "infos\contact.html")
     
